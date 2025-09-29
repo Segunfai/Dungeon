81aee060636645e10034259b9f5bbf6e7c6aa853
Segunfai <segunfai111@yandex.ru>
2025-09-30 00:53:49 +0500

    3

--- dungeon/src/Main.java
+++
@@ -1,8 +0,0 @@
-import core.Game;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Запуск DungeonMini...");
-        new Game().run();
-    }
-}
--- dungeon/src/core/InvalidCommandException.java
+++
@@ -1,7 +0,0 @@
-package core;
-
-public class InvalidCommandException extends RuntimeException {
-    public InvalidCommandException(String m) {
-        super(m);
-    }
-}
